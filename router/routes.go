d8f023c0e08afc71c30ef6c983f8261b9e3d68ac
Trajy <henrique.trajano.a@gmail.com>
2025-04-19 08:04:24 -0300

    fix: duplicated route with same http method

--- router/routes.go
+++ router/routes.go
@@ -9,7 +9,7 @@ func defineRoutes(r *gin.Engine) {
 	rv1 := r.Group("/api/v1")
 	{
 		rv1.GET("/opening", handler.Find)
-		rv1.GET("/opening", handler.FindById)
+		//rv1.GET("/opening", handler.FindById)
 		rv1.POST("/opening", handler.Create)
 		rv1.PUT("/opening", handler.Update)
 		rv1.DELETE("/opening", handler.Delete)
